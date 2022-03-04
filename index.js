56876494ec7ef0b2065bc26992bc1cd144b4ca86
Kashane S <ksakhakorn@gmail.com>
2022-03-03 16:31:52 -0800

    nearly finished the application. generateMarkdown completed. index completed. readme almost completed. planning to resume work on this.

--- index.js
+++ index.js
@@ -1,42 +1,74 @@
 // TODO: Include packages needed for this application
 const fs = require('fs')
 const inquirer = require('inquirer')
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [
+const promptQuestions = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Give a description of your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Are there any installation instructions?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'What is the usage information?',
+    },
+    {
+      type: 'input',
+      name: 'guidelines',
+      message: 'What are the guidelines for contributing to your project?',
+    },
+    {
+      type: 'input',
+      name: 'testing',
+      message: 'Describe the test instructions:',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Pick from this list of licenses:',
+      choices: ['Apache', 'Boost', 'Eclipse', 'IBM',  'MIT', 'Mozilla', 'ODbl', 'PDDL']
+    },
+    {
+      type: 'input',
+      name: 'gitHubUser',
+      message: 'What is your GitHub Username?',
+    },
+    {
+      type: 'input',
+      name: 'userEmail',
+      message: 'What is your preferred email address?'
+    },
+  ]);
+};
 
-];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-
+  fs.writeFileSync(fileName, generateMarkdown(data) 
+  )
 }
 
 // TODO: Create a function to initialize app
-function init() {
-
-    // i think for loop on the array would go here?
-
-    // and i think at the end of this will be the function, writeToFile(fileName, data) that was defined above
-}
+const init = () => {
+  promptQuestions()
+    .then((data) => writeToFile('README.md', data))
+    .then(() => console.log('Successfully wrote to README.md'))
+    .catch((err) => console.error(err));
+};
 
 // Function call to initialize app
 init();
-
-
-
-//ideas for later:
-// create an array of all the questions that will be asked to the user
-// then a for loop on that array that includes inquirer 
-// 2. Using multidimensional arrays
-const moreSongs = [
-    ['Bad Guy', 1],
-    ['The Wheels on the Bus', 2],
-    ['Friday', 3],
-  ];
-  
-  // for of loop
-  for (const song of moreSongs) {
-    console.log(song);
-  }
-  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,106 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license){
+    case 'Apache':
+      return 'https://img.shields.io/badge/License-Apache_2.0-blue.svg'
+      break;
+    case 'Boost':
+      return 'https://img.shields.io/badge/License-Boost_1.0-lightblue.svg'
+      break;
+    case 'Eclipse':
+      return 'https://img.shields.io/badge/License-EPL_1.0-red.svg'
+      break;
+    case 'IBM':
+      return 'https://img.shields.io/badge/License-IPL_1.0-blue.svg'
+      break;
+    case 'MIT':
+      return 'https://img.shields.io/badge/License-MIT-yellow.svg'
+      break;
+    case 'Mozilla':
+      return 'https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg'
+      break;
+    case 'ODbl':
+      return 'https://img.shields.io/badge/License-ODbL-brightgreen.svg'
+      break;
+    case 'PDDL':
+      return 'https://img.shields.io/badge/License-PDDL-brightgreen.svg'
+      break;
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license){
+    case 'Apache':
+      return 'https://opensource.org/licenses/Apache-2.0'
+      break;
+    case 'Boost':
+      return 'https://www.boost.org/LICENSE_1_0.txt'
+      break;
+    case 'Eclipse':
+      return 'https://opensource.org/licenses/EPL-1.0'
+      break;
+    case 'IBM':
+      return 'https://opensource.org/licenses/IPL-1.0'
+      break;
+    case 'MIT':
+      return 'https://opensource.org/licenses/MIT'
+      break;
+    case 'Mozilla':
+      return 'https://opensource.org/licenses/MPL-2.0'
+      break;
+    case 'ODbl':
+      return 'https://opendatacommons.org/licenses/odbl/'
+      break;
+    case 'PDDL':
+      return 'https://opendatacommons.org/licenses/pddl/'
+      break;
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return `[![License: ${license}](${renderLicenseBadge(license)})](${renderLicenseLink(license)})`
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.title}
+${renderLicenseSection(data.license)}
+* [Description](#description)
+* [Installation Instructions](#installation-instructions)
+* [Usage Information](#usage-information)
+* [License](#license)
+* [Contribution Guidelines](#contribution-guidelines)
+* [Test Instructions](#test-instructions)
+* [Questions](#questions)
+---
+### Description
+${data.description}
+### Installation
+${data.installation}
+### Usage
+${data.usage}
+### License
+${renderLicenseLink(data.license)}
+### Contributing
+${data.guidelines}
+### Tests
+${data.testing}
+---
+#### Questions
+Find me on Github: https://github.com/${data.gitHubUser} 
 
+Or reach out to me with any questions or comments: ${data.userEmail}
 `;
 }
 
